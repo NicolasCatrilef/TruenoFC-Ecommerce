7db25abe799c00b52c4b26cd19971f96c3b66256
Nicolas Catrilef F <n.j.catrileflores@gmail.com>
2022-01-05 15:27:01 -0300

    feat: se mejora navbar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react'
-import { Image } from 'semantic-ui-react';
+import React, { useContext, useState } from 'react'
+import { Image, Input, Menu, Segment } from 'semantic-ui-react';
 import { CardWidget } from '../CartWidget.js/CardWidget';
 import logo from './Trueno-FC.ico'
 
@@ -13,35 +13,42 @@ import { CartContext } from '../Context/CartContext'
 
 export const NavBar = () => {
 
-	const [ items, setItems, removeItem, clear ] = useContext(CartContext);
+	const [ items, setItems ] = useContext(CartContext);
 	
+	const [option, setOption] = useState('initialState');
+	
+	const handleItemClick = ({ name }) => {
+		setOption(name)
+	}
+
     return (
-        <nav className='container background-red'>
-			<ul>
-				<li>
-					<Link to="/">
-						<Image src={logo} size='tiny' />
-					</Link>
-				</li>
-				<li>
-					<Link to='/category/camiseta'>
-						<p>Camisetas</p>
-					</Link>
-				</li>
-				<li>
-					<Link to='/category/accesorio'>
-						<p>Accesorios</p>
-					</Link>
-				</li>
-				<li>
-					{
-						items.length > 0 &&
-							<Link to='/cart'>
-								<CardWidget className='CardWidget'/>
-							</Link>
-					}
-				</li>
-			</ul>
-		</nav>
+		<div className='container background-red'>
+			<Menu attached='top' tabular>
+				<Menu.Menu position='left'>
+					<Link to="/">	
+						<Menu.Item>
+							<Image src={logo} size='tiny' />
+						</Menu.Item>
+					</ Link>
+				</Menu.Menu>
+				
+				<Link to='/category/camiseta'class="item">
+					<Menu.Item name='Camisetas' active={option === 'Camisetas'} onClick={handleItemClick} />
+				</ Link>
+				<Link to='/category/accesorio' class="item">
+					<Menu.Item name='Accesorios' active={option === 'Accesorios'} onClick={handleItemClick} />
+				</ Link>
+				
+				{ Number(items.length) > 0 &&
+					<Menu.Menu position='right'>
+						<Link to='/cart' class='item'>
+								<Menu.Item>
+									<CardWidget className='CardWidget'/>
+								</Menu.Item>
+						</ Link>
+					</Menu.Menu>
+				}
+			</Menu>
+      	</div>
     )
 }
